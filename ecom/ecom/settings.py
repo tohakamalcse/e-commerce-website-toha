88f3b96a5cc9b387ab45349a768671bcd33ac35a
toha <tohakamalcse@gmail.com>
2025-11-10 04:05:52 +0000

    Update files for Render deployment

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -1,38 +1,48 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-# -----------------------------
-# BASE DIRECTORY
-# -----------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# -----------------------------
-# LOAD .ENV FILE (LOCAL DEV)
-# -----------------------------
-# This will load your local .env file. In production (Render/Docker), system env vars will be used.
-load_dotenv(BASE_DIR / ".env")
 
 # -----------------------------
 # SECRET KEY & DEBUG
 # -----------------------------
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-default-secret-key'
-)
-
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # -----------------------------
 # ALLOWED HOSTS
 # -----------------------------
-ALLOWED_HOSTS = [
-    host.strip() for host in os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
-    if host.strip()
-]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 # -----------------------------
-# APPLICATIONS
+# DATABASE
+# -----------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+
+# -----------------------------
+# STATIC FILES
+# -----------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# -----------------------------
+# MEDIA FILES
+# -----------------------------
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# -----------------------------
+# APPS & MIDDLEWARE
 # -----------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,9 +54,6 @@ INSTALLED_APPS = [
     'store',
 ]
 
-# -----------------------------
-# MIDDLEWARE
-# -----------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -58,44 +65,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# -----------------------------
-# URLS & WSGI
-# -----------------------------
 ROOT_URLCONF = 'ecom.urls'
-
 WSGI_APPLICATION = 'ecom.wsgi.application'
-
-# -----------------------------
-# TEMPLATES
-# -----------------------------
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-# -----------------------------
-# DATABASE
-# -----------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'ecom_db'),
-        'USER': os.environ.get('DB_USER', 'toha'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', '1234'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
-}
 
 # -----------------------------
 # PASSWORD VALIDATION
@@ -115,23 +86,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# -----------------------------
-# STATIC FILES
-# -----------------------------
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# -----------------------------
-# MEDIA FILES
-# -----------------------------
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-# -----------------------------
-# DEFAULT AUTO FIELD
-# -----------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # -----------------------------
